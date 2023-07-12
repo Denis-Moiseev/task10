c01e58de774b93560f76a5488545b1d112ea1d13
Denis-Moiseev <mrtesla666@gmail.com>
2023-07-13 00:14:09 +0800

    finished

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
 ﻿Console.Write("Введите трёхзначное число: ");
 int number = int.Parse(Console.ReadLine());
 int secondDigit = (number / 10) % 10;
-Console.Write(secondDigit);
+Console.Write($"Вторая цифра: {secondDigit}");
